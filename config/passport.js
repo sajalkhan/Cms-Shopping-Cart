b2ce286185238a4d826f89abc9cfb05bae36a078
sajalkhan <sajalhossen@yahoo.com>
2020-01-04 13:11:45 +0600

    fix 2

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,5 @@
 var LocalStrategy = require('passport-local').Strategy;
-var User = require('../models/user_model');
+var User = require('../Models/user_model');
 var bcrypt = require('bcrypt');
 
 module.exports = function (passport) {
